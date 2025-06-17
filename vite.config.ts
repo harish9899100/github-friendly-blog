e29c865166f4b6cb88058d6039b59d100b8b4200
harish khileri <harishkhilery98@gmail.com>
2025-06-17 16:53:12 +0530

    modified some files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  base: '/github-friendly-blog/',
+  base: '/github-friendly-blog/', // update based on your repo name
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
